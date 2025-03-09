cd1586f8b769825f826ecdc7e5e6c45abab8b5e9
ThuTa4 <hyatmyat779@gmail.com>
2025-03-09 20:31:05 +0630

    add: fixed file

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -16,31 +16,124 @@ const boardGrid = [
   [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20], // Bottom row
 ];
 
+//grid-rows-[110px,repeat(9,85px),110px]
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center">
-      <div className="w-[1000px] h-[1000px] bg-blue-50 grid grid-cols-11 grid-rows-11 gap-0 border-4 border-black  ">
+    <main className="flex items-center justify-center p-4">
+      <div
+        style={{
+          gridTemplateRows: "100px repeat(9, 75px) 100px", // First & Last = 110px, Middle Rows = 85px
+        }}
+        className="grid grid-flow-col gap-0 rounded-[10px] border-2 border-gray-800 "
+      >
         {boardGrid?.flat().map((index, i) =>
           index === null ? (
-            <div className=" bg-amber-500 text-black ">{i}</div>
-          ) : monoData[index].type === "special" ? (
+            <div
+              key={i}
+              style={{ display: "contents" }}
+              className=" w-0 h-0 bg-gray-300 border "
+            >
+              <div></div>
+            </div> // Empty space
+          ) : monoData[index]?.type === "special" ? (
             <div
               key={index}
               style={{ background: monoData[index].color }}
-              className={`  min-w-[100px]  max-w-[100px] max-h-[100px] h-[100px]`}
+              className="max-w-[100px] !min-h-[100px] flex items-center justify-center border text-center text-[11px] font-medium"
             >
-              {monoData[index].name}
+              <div className="w-full h-full object-cover ">
+                <img
+                  src={monoData[index].cityImage}
+                  className="w-full h-full object-cover "
+                />
+              </div>
+            </div>
+          ) : index < 10 ? (
+            <div
+              key={index}
+              style={{ background: monoData[index].color }}
+              className="w-[100px] h-[75px] flex items-center justify-center   border text-center text-[10px] font-medium relative"
+            >
+              <div className="absolute left-0 max-h-[75px] w-auto -rotate-90 z-50">
+                {monoData[index].name}
+              </div>
+
+              <div className="w-[75px] h-[100px] object-cover -rotate-90">
+                <img
+                  src={monoData[index].cityImage}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          ) : index > 20 && index < 30 ? (
+            <div
+              key={index}
+              style={{ background: monoData[index].color }}
+              className="w-[100px] h-[75px] flex items-center justify-center border text-center text-[10px] font-medium overflow-hidden relative"
+            >
+              <div className="absolute right-0 max-h-[75px] w-auto rotate-90 z-50">
+                {monoData[index].name}
+              </div>
+
+              <div className="w-[75px] h-[100px] object-cover rotate-90">
+                <img
+                  src={monoData[index].cityImage}
+                  className="w-full h-full object-cover"
+                />
+              </div>
             </div>
           ) : (
             <div
               key={index}
               style={{ background: monoData[index].color }}
-              className={`  min-w-[100px]  max-w-[100px] max-h-[100px] h-[100px]`}
+              className="w-[75px] h-[100px] flex items-center justify-center border text-center text-[10px] font-medium  relative  bg-amber-300"
             >
-              {monoData[index].name}
+              <div className=" absolute top-0 text-center">
+                {monoData[index].name}
+              </div>
+              <div className="w-full h-full object-cover">
+                <img
+                  src={monoData[index].cityImage}
+                  className="w-full h-full object-cover"
+                />
+              </div>
             </div>
           )
         )}
+      </div>
+      <div className="absolute">
+        <div className="relative  w-[105px] h-[105px] bottom-50 transform-3d rotate-x-45 rotate-y-45">
+          <div
+            id="front"
+            style={{ transform: "translateZ(50px)" }}
+            className="face after:content-[''] after:absolute after:w-[30px] after:h-[30px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#f63330] after:-mt-[15px] after:-ml-[15px]"
+          ></div>
+          <div
+            id="back"
+            style={{ transform: "rotateX(180deg) translateZ(50px)" }}
+            className="face after:content-[''] after:absolute after:w-[17px] after:h-[17px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#131210] after:-mt-[35px] after:-ml-[30px] after:shadow-[40px_0_#131210,0_25px_#131210,40px_25px_#131210,0_50px_#131210,40px_50px_#131210]"
+          ></div>
+          <div
+            id="top"
+            style={{ transform: "rotateX(90deg) translateZ(50px)" }}
+            className="face  after:content-[''] after:absolute after:w-[17px] after:h-[17px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#131210] after:-mt-[30px] after:-ml-[30px] after:shadow-[40px_40px_#131210]"
+          ></div>
+          <div
+            id="bottom"
+            style={{ transform: "rotateX(-90deg) translateZ(50px)" }}
+            className="face after:content-[''] after:absolute after:w-[17px] after:h-[17px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#131210] after:-mt-[36px] after:-ml-[36px] after:shadow-[26px_26px_#131210,52px_52px_#131210,52px_0_#131210,0_52px_#131210]"
+          ></div>
+          <div
+            id="right"
+            style={{ transform: "rotateY(90deg) translateZ(50px)" }}
+            className="face after:content-[''] after:absolute after:w-[17px] after:h-[17px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#131210] after:-mt-[30px] after:-ml-[30px] after:shadow-[40px_0_#131210,0_40px_#131210,40px_40px_#131210]"
+          ></div>
+          <div
+            id="left"
+            style={{ transform: "rotateY(-90deg) translateZ(50px)" }}
+            className="face after:content-[''] after:absolute after:w-[17px] after:h-[17px] after:rounded-[50%] after:top-[50%] after:left-[50%] after:bg-[#131210] after:-mt-[35px] after:-ml-[35px] after:shadow-[25px_25px_#131210,50px_50px_#131210]"
+          ></div>
+        </div>
       </div>
     </main>
   );
